9b518b155a00c20d6db394013f97d96cce8c7b20
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2023-02-02 15:44:15 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+
+import Home from './components/HomePage '
+import ListBeers from './components/ListBeers'
+import RandomBeer from './components/RandomBeer'
+import NewBeer from './components/NewBeer'
+import SingleBeer from './components/SingleBeer'
+
+
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/beers' element={<ListBeers />} />
+        <Route path='/beers/:beerId' element={<SingleBeer />} />
+        <Route path='/random-beer' element={<RandomBeer />} />
+        <Route path='/new-beer' element={<NewBeer />} />
+       
+      </Routes>
+      
     </div>
   );
 }
